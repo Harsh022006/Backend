834315228dc57a877257b252e72183535a77812e
Harsh022006 <harshamburkar@gmail.com>
2025-10-08 01:13:50 +0530

    assignment

---
+++ ASSIGNMENTS/Python-Fundamentals_Of_Python_Language/LAB-EXERCISE/Topic-9_&_10/Demonstrate_String_Slicing.py
@@ -0,0 +1,16 @@
+text = "PythonProgramming"
+
+# Slice from index 0 to 6
+print("Slice [0:6]:", text[0:6])
+
+# Slice from index 6 to end
+print("Slice [6:]:", text[6:])
+
+# Slice from beginning to index 10
+print("Slice [:10]:", text[:10])
+
+# Slice the whole string
+print("Slice [:]:", text[:])
+
+# Slice with step value (every 2nd character)
+print("Slice [::2]:", text[::2])
